d0794adab470e28442ce502751832cce22ccd79a
IsralMoralesA <111374390+IsralMoralesA@users.noreply.github.com>
2022-09-14 16:22:08 -0500

    Add files via upload

---
+++ Ejercicio/diff.mysql.sql
@@ -0,0 +1,39 @@
+Reference Database: israelmorales@localhost @ jdbc:mysql://localhost:3306/liquibase2?useSSL=false (Default Schema: liquibase2)
+Comparison Database: israelmorales@localhost @ jdbc:mysql://localhost:3306/LiquiBase?useSSL=false (Default Schema: LiquiBase)
+Compared Schemas: liquibase2 -> LiquiBase
+Product Name: EQUAL
+Product Version: EQUAL
+Missing Catalog(s): NONE
+Unexpected Catalog(s): NONE
+Changed Catalog(s): NONE
+Missing Column(s): NONE
+Unexpected Column(s): 
+     liquibase.address.calle
+     liquibase.address.codigopostal
+     liquibase.address.colonia
+     liquibase.address.delegacion
+     liquibase.address.idaddress
+     liquibase.address.numero
+Changed Column(s): NONE
+Missing Foreign Key(s): NONE
+Unexpected Foreign Key(s): NONE
+Changed Foreign Key(s): NONE
+Missing Index(s): NONE
+Unexpected Index(s): NONE
+Changed Index(s): NONE
+Missing Primary Key(s): NONE
+Unexpected Primary Key(s): NONE
+Changed Primary Key(s): NONE
+Missing Sequence(s): NONE
+Unexpected Sequence(s): NONE
+Changed Sequence(s): NONE
+Missing Table(s): NONE
+Unexpected Table(s): 
+     address
+Changed Table(s): NONE
+Missing Unique Constraint(s): NONE
+Unexpected Unique Constraint(s): NONE
+Changed Unique Constraint(s): NONE
+Missing View(s): NONE
+Unexpected View(s): NONE
+Changed View(s): NONE
---
+++ Ejercicio/diffchangelog.mysql.sql
@@ -0,0 +1,15 @@
+-- liquibase formatted sql
+
+-- changeset asmet.morales:1663190240498-1
+DROP TABLE address;
+
+-- liquibase formatted sql
+
+-- changeset asmet.morales:1663190285722-1
+DROP TABLE address;
+
+-- liquibase formatted sql
+
+-- changeset asmet.morales:1663190375375-1
+DROP TABLE address;
+
